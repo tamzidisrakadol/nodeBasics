4a0557345cd2c2f17e7d662b5ec8e158138b566b
Tamzid Israk <tamzidisrak12@gmail.com>
2023-05-31 00:53:14 +0600

    Node basics local modul

---
+++ Lesson1/localModul.js
@@ -0,0 +1,13 @@
+const s1 = require('./student');  
+
+console.log("hello");
+console.log(s1.getName());
+console.log(s1.getAge());
+
+
+
+/*terminal cmd 
+ pwd-> path 
+ ls-> show all files in folder
+ cd -> change directory
+*/ 
---
+++ Lesson1/student.js
@@ -0,0 +1,18 @@
+//es6
+
+const getName= () => {
+    return "Tamzid Isark";
+}
+
+const getAge= () => {
+    return "14";
+}
+
+// exports.getName = getName
+// exports.getAge = getAge
+
+
+module.exports={
+    getName,
+    getAge
+}
