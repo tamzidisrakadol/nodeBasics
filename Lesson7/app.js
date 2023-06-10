f5755539c55bc2353de77bd1465a8b5b0295bcad
Tamzid Israk <tamzidisrak12@gmail.com>
2023-06-10 06:34:17 +0600

    Http methods

--- Lesson7/app.js
+++ Lesson7/app.js
@@ -1,4 +1,24 @@
 const express = require('express')
 const app = express()
 
+//http methods = get(Read) , post(Create) , put(update) , delete(delete)
+
+app.get("/",(req,res)=>{
+    res.send("I am get request at home page")
+    res.end()
+})
+app.post("/",(req,res)=>{
+    res.send("I am post request at home page")
+    res.end()
+})
+app.put("/",(req,res)=>{
+    res.send("I am put request at home page")
+    res.end()
+})
+app.delete("/",(req,res)=>{
+    res.send("I am delete request at home page")
+    res.end()
+})
+
+
 module.exports = app
