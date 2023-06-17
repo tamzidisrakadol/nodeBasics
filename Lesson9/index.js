4949d42a9522c779563fa39cccb18edc030db835
Tamzid Israk <tamzidisrak12@gmail.com>
2023-06-18 03:41:57 +0600

    middleware , static middleware

--- Lesson9/index.js
+++ Lesson9/index.js
@@ -3,12 +3,25 @@ const express=require('express')
 const app= express()
 const PORT = process.env.PORT || 3000
 
+const myMiddleWare = (req,res,next)=>{
+    console.log("Middleware function")
+    req.currentTime = new Date(Date.now())
+    next()
+}
+app.use(myMiddleWare)
+app.use(express.static("public"))
+
 app.listen(PORT,()=>{
     console.log(`server is running at ${PORT}`)
 })
 
 app.get("/",(req,res)=>{
+    console.log("I am home"+req.currentTime)
     res.send("Hello I am home route")
+})
+
+app.get("/contact",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
 })
 
 app.use("*",(req,res)=>{
